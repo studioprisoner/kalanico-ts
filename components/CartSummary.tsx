b7ccb445007a88a03c819754e5dc2561ced4f2b3
Josh Illichmann <josh@studioprisoner.com>
2021-07-25 12:21:04 +1000

    fixed width and margin for cart

--- components/CartSummary.tsx
+++ components/CartSummary.tsx
@@ -36,7 +36,7 @@ const CartSummary = () => {
   }
 
   return (
-    <div className="px-4 pt-5 sm:px-6">
+    <div className="max-w-7xl mx-auto px-4 pt-5 sm:px-6">
     <div className="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap">
       <div className="ml-4 mt-4">
           <h3 className="text-lg leading-6 font-medium text-almond-900">Your Cart</h3>
